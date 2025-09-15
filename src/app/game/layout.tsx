0435384e1c27e59ccab25f82348a9fe195648504
SvatyJan <honzikraj@gmail.com>
2025-09-15 18:23:49 +0200

    reset
    reset

--- src/app/game/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { getCurrentUser } from "@/lib/auth";
-import { redirect } from "next/navigation";
-
-export default async function GameLayout({ children }: { children: React.ReactNode }) {
-  const user = await getCurrentUser();
-  if (!user) redirect("/login");
-  return <>{children}</>;
-}
